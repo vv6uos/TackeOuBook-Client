296738cdb0ea175a8bf1a67900253090c4b6ff71
vv6uos <wanamzz600@gmail.com>
2022-01-04 16:58:42 +0900

    헤더 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import "./App.css";
 import MainPage from "./main";
 import UploadPage from "./upload";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "./login";
-import React from "react";
+import Header from "./header";
 
 function App() {
-  const movePage = function () {
-    console.log("페이지이동");
-  };
   return (
     <div>
-      <div id="header">
-        <Link to="/" onClick={movePage}>
-          <img id="logo" src="/images/logo4.png" alt="로고이미지" />
-        </Link>
-        <div id="menu_box">
-          {/* 메뉴아이콘만들기  */}
-          <Link to="/login">
-            <img
-              className="menu-button"
-              src="/images/menu_button-login.jpg"
-              alt="회원가입/로그인"
-            />
-          </Link>
-          <Link to="/upload">
-            <img
-              className="menu-button"
-              src="images/menu_button-upload.jpg"
-              alt="상품올리기버튼 올리기"
-            />
-          </Link>
-        </div>
-      </div>
-      <div id="banner">
-        <img src="/images/banner-002.png" alt="배너" />
-      </div>
+      <Header />
+      
       <div id="body">
         <Routes>
           <Route path={"/upload"} element={<UploadPage />} />
--- src/main/index.js
+++ src/main/index.js
@@ -25,41 +25,44 @@ function MainPage() {
 
   return (
     <div>
-        <div id="bestsellerBook_board">
-          <h2>추천도서 (출처 : 알라딘 베스트셀러 추천도서 API) </h2>
-          <div id="bestseller_card_list">
-            {bestsellers.map((bestseller) => {
-              return (
-                <div className="bestseller_card">
-                  <img src={`${bestseller.imgURL}`} alt="베스트셀러 사진" />
-                  <div className="bestseller_info">
-                    <div>{bestseller.id}</div>
-                    <div>{bestseller.name}</div>
-                  </div>
+      <div id="banner">
+        <img src="/images/banner-002.png" alt="배너" />
+      </div>
+      <div id="bestsellerBook_board">
+        <h2>추천도서 (출처 : 알라딘 베스트셀러 추천도서 API) </h2>
+        <div id="bestseller_card_list">
+          {bestsellers.map((bestseller) => {
+            return (
+              <div className="bestseller_card">
+                <img src={`${bestseller.imgURL}`} alt="베스트셀러 사진" />
+                <div className="bestseller_info">
+                  <div>{bestseller.id}</div>
+                  <div>{bestseller.name}</div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
-        <div id="sale_book_board">
-          <h1>판매도서</h1>
-          <div id="saleBook_card_list">
-            {books.map((book) => {
-              return (
-                <div className="saleBook_card">
-                  <img src={`${book.imgURL}`} alt="판매도서사진" />
-                  <div className="saleBook_info">
-                    <p>{book.id}</p>
-                    <p>도 서 명:{book.name}</p>
-                    <p>판매가격:{book.price}원</p>
-                    <p>판 매 자:{book.seller}</p>
-                  </div>
+      </div>
+      <div id="sale_book_board">
+        <h1>판매도서</h1>
+        <div id="saleBook_card_list">
+          {books.map((book) => {
+            return (
+              <div className="saleBook_card">
+                <img src={`${book.imgURL}`} alt="판매도서사진" />
+                <div className="saleBook_info">
+                  <p>{book.id}</p>
+                  <p>도 서 명:{book.name}</p>
+                  <p>판매가격:{book.price}원</p>
+                  <p>판 매 자:{book.seller}</p>
                 </div>
-              );
-            })}
-          </div>
-          판매도서 카드 만들기 판매완료된 도서는 회색으로 솔드아웃표시{" "}
+              </div>
+            );
+          })}
         </div>
+        판매도서 카드 만들기 판매완료된 도서는 회색으로 솔드아웃표시{" "}
+      </div>
     </div>
   );
 }
