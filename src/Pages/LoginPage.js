336622a1ce8657fbbd8f745d1c2c8be9e97abd34
vv6uos <wanamzz600@gmail.com>
2022-02-06 00:32:32 +0900

    로그인 데이터 서버 전달

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,23 +1,62 @@
 //-----import 외부 소스
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import axios from "axios";
 
 //-----import 내부 소스
 import { InputWithLabel, Button } from "components/index";
 import { myCSS, myTheme } from "style/index";
+import { API_URL } from "config/constants";
 
 const { colors, fontStyles } = myTheme;
 
 function LoginPage() {
+  const [id, setId] = useState(null);
+  const [password, setPassword] = useState(null);
+
+  const values = { id, password };
+
+  const onIdHandler = (e) => {
+    setId(e.currentTarget.value);
+  };
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${API_URL}/users/login`, {
+        user_id: values.id,
+        password: values.password,
+      })
+      .then((result) => {
+        alert("로그인 성공");
+      })
+      .catch((err) => {
+        alert("로그인 실패!");
+      });
+  };
   return (
     <Wrapper>
       <Container>
         <Title>로그인</Title>
-        <Form>
-          <InputWithLabel type="text" required>
+        <Form onSubmit={onSubmit}>
+          <InputWithLabel
+            type="text"
+            value={id}
+            onChange={onIdHandler}
+            required
+          >
             아이디
           </InputWithLabel>
-          <InputWithLabel type="password" required>
+          <InputWithLabel
+            type="password"
+            value={password}
+            onChange={onPasswordHandler}
+            required
+          >
             비밀번호
           </InputWithLabel>
           <Button type="submit" width="20rem" maxWidth="250px">
@@ -29,6 +68,7 @@ function LoginPage() {
     </Wrapper>
   );
 }
+
 export default LoginPage;
 
 //-----스타일
