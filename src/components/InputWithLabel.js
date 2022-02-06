b43b3daa64ece9faa5dd28c44327bfedd845b428
vv6uos <wanamzz600@gmail.com>
2022-02-07 00:24:43 +0900

    <Input> : 조건부 invalid 스타일링 추가

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,27 +1,41 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { useState } from "react";
 
-export default function InputWithLabel({ children, ...rest }) {
+export default function InputWithLabel({
+  children,
+  flexDirection,
+  labelWidth,
+  itemHeight,
+
+  ...rest
+}) {
+  const [mode, setMode] = useState("default");
+
+  function onChangeMode() {
+    setMode("confirm");
+  }
   return (
-    <Box>
-      <Label>{children}</Label>
-      <Input {...rest} />
-    </Box>
+    <Item flexDirection={flexDirection} itemHeight={itemHeight}>
+      <Label labelWidth={labelWidth} itemHeight>
+        {children}
+      </Label>
+      <Input $invalidmode={mode} onKeyUp={onChangeMode} {...rest} />
+    </Item>
   );
 }
 
-const Box = styled.label`
+const Item = styled.div`
   display: flex;
-  height: 2rem;
+  flex-direction: ${(p) => p.flexDirection || "row"};
+  height: ${(p) => p.itemHeight || "2rem"};
   font-size: 1.2rem;
   max-width: 300px;
-
-  & + & {
-    margin: 0.5rem;
+  margin: 0.6rem;
   }
 `;
 
 const Label = styled.label`
-  width: 6.6rem;
+  width: ${(p) => p.labelWidth || "6.6rem"};
   min-width: 60px;
   color: gray;
   line-height: 2rem;
@@ -33,6 +47,22 @@ const Input = styled.input`
   appearance: none;
   outline-style: none;
   border: none;
-  width: 13.3rem;
+  width: ${(p) => p.inputWidth || "13.3rem"};
   font-size: 1rem;
+  height: 2rem;
+
+  :invalid {
+    ${(p) => {
+      switch (p.$invalidmode) {
+        case "confirm":
+          return css`
+            border: 1px solid red;
+          `;
+        default:
+          return css`
+            border: none;
+          `;
+      }
+    }}
+  }
 `;
