f2d46b875b0b1c142f37ffe6b77c897fbc3f7296
vv6uos <wanamzz600@gmail.com>
2022-02-18 06:35:53 +0900

    마이링크 컴포넌트 CSS수정

--- src/components/MyLink.js
+++ src/components/MyLink.js
@@ -1,10 +1,27 @@
 import { Link } from "react-router-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-function MyLink({ children, to }) {
-  return <StyledLink to={to}>{children}</StyledLink>;
+import { myTheme } from "style/index";
+
+function MyLink({ children, to, fontColor, margin }) {
+  return (
+    <StyledLink to={to} color={fontColor} margin={margin}>
+      {children}
+    </StyledLink>
+  );
 }
 export default MyLink;
+
+const { colors, fontStyles } = myTheme;
+
 const StyledLink = styled(Link)`
+  &:visited {
+    color: ${(p) => p.color || colors.d2};
+    text-decoration: none;
+  }
+  ${fontStyles.smbold};
+  margin: ${(p) => p.margin || "0"};
+
+  color: ${(p) => p.color || colors.d2};
   text-decoration: none;
 `;
