b2a2a4114d71a2603a44c1e7577d967b7eb2c06d
vv6uos <wanamzz600@gmail.com>
2022-02-17 23:54:07 +0900

    마이페이지 Route 생성

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   UploadPage,
   TestPage,
   Header,
+  MyPage,
 } from "Pages/index";
 import { myCSS, myTheme, GlobalStyle } from "style/index";
 
@@ -56,6 +57,7 @@ function App() {
             <Route path={"/test"} element={<TestPage />} />
             <Route path={"/login"} element={<LoginPage />} />
             <Route path={"/register"} element={<RegisterPage />} />
+            <Route path={"/mypage"} element={<MyPage />} />
           </Routes>
         </Body>
         <Footer>@s6uos ALL RIGHTS RESERVED</Footer>
