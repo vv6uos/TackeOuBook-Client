971884ef8ae7403f5def1fcf2eded4945dba8f6a
vv6uos <wanamzz600@gmail.com>
2022-02-22 04:55:46 +0900

    구독취소버튼 로직 작성

--- src/Pages/Mypage/components/MySubscribe.js
+++ src/Pages/Mypage/components/MySubscribe.js
@@ -1,14 +1,28 @@
 //-----import 외부 소스
-import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
 import React, { useState, useEffect } from "react";
+import { Button } from "components";
 import axios from "axios";
 import { API_URL } from "config/constants";
-import { MyLink } from "components";
-import { myCSS, myTheme } from "style";
+import onChangeSubscribe from "components/Modals/Subscribe/onChangeSubscribe";
 
-function MySubscribe() {
-  return <div>구독 페이지 로드</div>;
+function MySubscribe({ isMember }) {
+  const { id, subscribe } = isMember;
+  const onCancelSubscribe = () => {
+    onChangeSubscribe(id, false);
+  };
+
+  return (
+    <Wrapper>
+      <h1>{`나의구독 ${subscribe}`}</h1>
+      <Button onClick={onCancelSubscribe}>구독취소</Button>
+    </Wrapper>
+  );
 }
 
 export default MySubscribe;
+
+const Wrapper = styled.div`
+  background-color: blue;
+  display: flex;
+`;
