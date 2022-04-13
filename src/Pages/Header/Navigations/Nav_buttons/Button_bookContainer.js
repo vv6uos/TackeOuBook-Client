fba9d42326e34f8d2e236d81100bed43e7a7b03c
vv6uos <wanamzz600@gmail.com>
2022-04-13 13:02:57 +0900

    리팩토링 북컨테이너 아이콘 -미사용 코드 삭제 -주석 추가

--- src/Pages/Header/Navigations/Nav_buttons/Button_bookContainer.js
+++ src/Pages/Header/Navigations/Nav_buttons/Button_bookContainer.js
@@ -1,10 +1,9 @@
 //-----import 외부
 import styled from "styled-components";
-import { useParams } from "react-router-dom";
 //-----import 내부
 import { MyLink } from "components";
 
-//-----메인 네비게이션의 책장아이콘 컴포넌트
+//-----메인 네비게이션의 책장아이콘: 링크하면 회원의 대여현황으로 이동
 function BookContainer({ userId }) {
   return (
     <MyLink to={`/mypage/${userId}/myrental`}>
